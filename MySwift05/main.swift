ebc5df4e55c08f76d7c58a5fafb7317606e5b2f7
Brad <brad@brad.tw>
2017-09-06 15:35:19 +0800

    Initial Commit

---
+++ MySwift05/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MySwift05
+//
+//  Created by user22 on 2017/9/6.
+//  Copyright © 2017年 Brad Big Company. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
