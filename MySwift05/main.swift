a570ae44a23ed4deeff1e176f75b782261fda583
Brad <brad@brad.tw>
2017-09-08 13:30:21 +0800

    2017090803

--- MySwift05/main.swift
+++ MySwift05/main.swift
@@ -14,3 +14,28 @@ myary()
 ary2test()
 poker()
 
+var v1 = 12 // Int
+var v2 = 9.0   // Double
+//if v1 == v2 {
+//    
+//}
+var v3:UInt = 12
+var v4:Int = 12
+if v1 == v4 {
+}
+var v5:Float = 12.0
+//if v2 == v5 {
+//}
+var v6 = 3.0
+print(v2.remainder(dividingBy: 5.321))
+print(v2.squareRoot())
+var v7 = 12.456
+
+mystr()
+
+
+
+
+
+
+
---
+++ MySwift05/mystr.swift
@@ -0,0 +1,81 @@
+//
+//  mystr.swift
+//  MySwift05
+//
+//  Created by user22 on 2017/9/8.
+//  Copyright © 2017年 Brad Big Company. All rights reserved.
+//
+
+import Foundation
+
+func mystr(){
+    var name = "BradChao"
+    print(type(of:name))
+    
+    name += "趙令文"
+    print(name)
+    print(name.unicodeScalars.count)
+    print(name.utf16.count)
+    print(name.utf8.count)
+    
+    for c in name.characters {
+        print(c)
+    }
+    
+    let chars = Array(name.characters)
+    print(type(of:chars))
+    print(chars.count)
+    print(chars[10])
+    
+    let newname = String(chars)
+    print(type(of:newname))
+    print(newname)
+
+    let cs1 = ["A","B","C","D"]
+    print(type(of:cs1))
+    let cs2:[Character] = ["A","B","C","D"]
+    print(type(of:cs2))
+
+    let ns2 = String(cs2)
+    print(ns2)
+    
+    var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    print(letters)
+    print(letters.unicodeScalars.count)
+    
+    let r1 = getStringRange(source: letters, from: 4, to: 9)
+    print(letters.substring(with: r1))
+    print(mysubstrv2(source: letters, from: 4, to: 9))
+    
+    let range = getStringRange(source: letters, from: 4, to: 9)
+    letters.replaceSubrange(range, with: "1234")
+    print(letters)
+    
+    let r2 = getStringRange(source: letters, from: 4, to: 8)
+    letters.removeSubrange(r2)
+    print(letters)
+    
+    
+    
+    
+}
+
+func getStringRange(source:String, from:Int, to:Int) -> Range<String.Index> {
+    let start = source.index(source.startIndex, offsetBy: from)
+    let end = source.index(source.startIndex, offsetBy: to)
+    let range = start..<end
+    return range
+}
+//func mysubstr(source:String, from:Int, to:Int) -> String {
+//    let range = getStringRange(source: source, from: from, to: to)
+//    return source.substring(with: range)
+//}
+
+func mysubstrv2(source:String, from:Int, to:Int) -> String {
+    let cs = Array(source.characters);
+    var ret = ""
+    for i in from..<to {
+        ret += String(cs[i])
+    }
+    return ret
+}
